e2cabab15da5ea0fa2f22ac39878060b781cc442
Gustavo Lemos <dev.gustavolemos@outlook.com>
2022-10-28 20:34:37 -0300

    Created login flow.

--- app/src/main/java/com/qpedido/android/activity/LoginActivity.java
+++ app/src/main/java/com/qpedido/android/activity/LoginActivity.java
@@ -1,14 +1,55 @@
 package com.qpedido.android.activity;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import com.qpedido.android.R;
+import com.qpedido.android.fragment.LoginFragment;
+import com.qpedido.android.fragment.RegisterFragment;
+
+import static java.security.AccessController.getContext;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView title;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        title = findViewById(R.id.textViewTitle);
+        title.setText(R.string.user_login);
+        loadLoginFragment();
+    }
+
+    private void loadLoginFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentLogin, new LoginFragment());
+        fragmentTransaction.commit();
+    }
+
+    public void onClickReturn(View view) {
+        finish();
+    }
+
+    public void onClickSwitchRegister(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentLogin, new RegisterFragment());
+        fragmentTransaction.commit();
+        title.setText(R.string.user_registration);
+    }
+
+    public void onClickSwitchLogin(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentLogin, new LoginFragment());
+        fragmentTransaction.commit();
+        title.setText(R.string.user_login);
     }
 }
---
+++ app/src/main/java/com/qpedido/android/fragment/LoginFragment.java
@@ -0,0 +1,27 @@
+package com.qpedido.android.fragment;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.FragmentTransaction;
+import com.qpedido.android.R;
+
+import java.util.Objects;
+
+public class LoginFragment extends Fragment  {
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
